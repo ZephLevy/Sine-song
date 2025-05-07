c1e61aae528718bcb33cd5fbe960f97b50e79fc6
Zeph <write2zeph@gmail.com>
2025-05-07 12:09:43 +0200

    Improve encapsulation which will be helpful with multiple voices

--- internal/songgenerator/generate.go
+++ internal/songgenerator/generate.go
@@ -1,11 +1,5 @@
 package songgenerator
 
-import (
-	"fmt"
-	"log"
-	"math"
-)
-
 type note struct {
 	duration float64
 	name     string
@@ -13,8 +7,7 @@ type note struct {
 
 func GetSong(sampleRateInt int) []int16 {
 	sampleRate := float64(sampleRateInt)
-	var frequencies []float64
-	notes := []note{
+	voice1 := []note{
 		{duration: 1, name: "D5"},
 		{duration: 1, name: "B4"},
 		{duration: 1, name: "G4"},
@@ -26,25 +19,6 @@ func GetSong(sampleRateInt int) []int16 {
 		{duration: 1 / 3.0, name: "G4"},
 		{duration: 2, name: "D4"},
 	}
-	fmt.Println(notes)
-	for _, note := range notes {
-		frequency, err := getFrequency(note.name)
-		if err != nil {
-			// Assume a typo or smth equivalent
-			// Still, I'm printing to let the user know
-			log.Println("Error getting frequency:", err)
-			continue
-		}
-		for i := range int(note.duration * sampleRate) {
-			_ = i
-			frequencies = append(frequencies, frequency)
-		}
-	}
-
-	var samples []int16
-	for i, frequency := range frequencies {
-		sample := int16(math.Sin(2.0*math.Pi*frequency*float64(i)/sampleRate) * 32767)
-		samples = append(samples, sample)
-	}
+	samples := getSamples(voice1, sampleRate)
 	return samples
 }
---
+++ internal/songgenerator/sampler.go
@@ -0,0 +1,30 @@
+package songgenerator
+
+import (
+	"log"
+	"math"
+)
+
+func getSamples(notes []note, sampleRate float64) []int16 {
+	var frequencies []float64
+	for _, note := range notes {
+		frequency, err := getFrequency(note.name)
+		if err != nil {
+			// Assume a typo or smth equivalent
+			// Still, I'm printing to let the user know
+			log.Println("Error getting frequency:", err)
+			continue
+		}
+		for i := range int(note.duration * sampleRate) {
+			_ = i
+			frequencies = append(frequencies, frequency)
+		}
+	}
+
+	var samples []int16
+	for i, frequency := range frequencies {
+		sample := int16(math.Sin(2.0*math.Pi*frequency*float64(i)/sampleRate) * 32767)
+		samples = append(samples, sample)
+	}
+	return samples
+}
