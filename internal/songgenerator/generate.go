35b9123e82d586f1d7f57e4f4622cce20dce409f
Zeph <write2zeph@gmail.com>
2025-05-07 12:58:56 +0200

    Add support for multiple voices

--- internal/songgenerator/generate.go
+++ internal/songgenerator/generate.go
@@ -19,6 +19,37 @@ func GetSong(sampleRateInt int) []int16 {
 		{duration: 1 / 3.0, name: "G4"},
 		{duration: 2, name: "D4"},
 	}
-	samples := getSamples(voice1, sampleRate)
+	voice2 := []note{
+		{duration: 1 / 3.0, name: "G3"},
+		{duration: 1 / 3.0, name: "B3"},
+		{duration: 1 / 3.0, name: "B3"},
+		{duration: 1 / 3.0, name: "G3"},
+		{duration: 1 / 3.0, name: "B3"},
+		{duration: 1 / 3.0, name: "B3"},
+		{duration: 1 / 3.0, name: "G3"},
+		{duration: 1 / 3.0, name: "B3"},
+		{duration: 1 / 3.0, name: "B3"},
+		{duration: 1 / 3.0, name: "G3"},
+		{duration: 1 / 3.0, name: "B3"},
+		{duration: 1 / 3.0, name: "B3"},
+		{duration: 1 / 3.0, name: "C3"},
+		{duration: 1 / 3.0, name: "E3"},
+		{duration: 1 / 3.0, name: "E3"},
+		{duration: 1 / 3.0, name: "C3"},
+		{duration: 1 / 3.0, name: "E3"},
+		{duration: 1 / 3.0, name: "E3"},
+		{duration: 1 / 3.0, name: "G3"},
+		{duration: 1 / 3.0, name: "B3"},
+		{duration: 1 / 3.0, name: "B3"},
+		{duration: 1 / 3.0, name: "G3"},
+		{duration: 1 / 3.0, name: "B3"},
+		{duration: 1 / 3.0, name: "B3"},
+	}
+	samples1 := getSamples(voice1, sampleRate)
+	samples2 := getSamples(voice2, sampleRate)
+	var samples []int16
+	for i := range samples1 {
+		samples = append(samples, samples1[i]+samples2[i])
+	}
 	return samples
 }
--- internal/songgenerator/sampler.go
+++ internal/songgenerator/sampler.go
@@ -23,7 +23,10 @@ func getSamples(notes []note, sampleRate float64) []int16 {
 
 	var samples []int16
 	for i, frequency := range frequencies {
-		sample := int16(math.Sin(2.0*math.Pi*frequency*float64(i)/sampleRate) * 32767)
+		// HACK - 2 is the number of voices
+		// It probably shouldn't be hardcoded
+		sample := int16(math.Sin(2.0*math.Pi*frequency*float64(i)/sampleRate) * (32767 / 2))
+
 		samples = append(samples, sample)
 	}
 	return samples
