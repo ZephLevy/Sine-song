dce074885b55ee393e75c2cb7f3572377c305727
Zeph <write2zeph@gmail.com>
2025-05-07 18:03:21 +0200

    Use a phase variable to stop popping

--- internal/songgenerator/sampler.go
+++ internal/songgenerator/sampler.go
@@ -6,7 +6,9 @@ import (
 )
 
 func getSamples(notes []note, sampleRate float64) []int16 {
-	var frequencies []float64
+	var samples []int16
+	var phase float64
+
 	for _, note := range notes {
 		frequency, err := getFrequency(note.name)
 		if err != nil {
@@ -15,19 +17,28 @@ func getSamples(notes []note, sampleRate float64) []int16 {
 			log.Println("Error getting frequency:", err)
 			continue
 		}
-		for i := range int(note.duration * sampleRate) {
-			_ = i
-			frequencies = append(frequencies, frequency)
+
+		// What happens when we just use i is that
+		// when the note changes, it made a popping sound.
+		// This was because i got reset for each note,
+		// so a peak in one note could abruptly change to a
+		// trough.
+		// Here, we use a phase variable to keep track of it between notes
+
+		numSamples := int(note.duration * sampleRate)
+		phaseIncrement := 2.0 * math.Pi * frequency / sampleRate
+
+		for range numSamples {
+			sample := int16(math.Sin(phase) * (32767 / 3))
+			samples = append(samples, sample)
+			phase += phaseIncrement
+
+			// I don't know how I managed to have the foresight
+			// to think of this, but it seems like a good idea
+			if phase > 2*math.Pi {
+				phase -= 2 * math.Pi
+			}
 		}
-	}
-
-	var samples []int16
-	for i, frequency := range frequencies {
-		// HACK - 3 is the number of voices
-		// It probably shouldn't be hardcoded
-		sample := int16(math.Sin(2.0*math.Pi*frequency*float64(i)/sampleRate) * (32767 / 3))
-
-		samples = append(samples, sample)
 	}
 	return samples
 }
