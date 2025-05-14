e4e4586ab9df7c37e17cfa93c777e5325d8532ad
Zeph <write2zeph@gmail.com>
2025-05-14 21:36:14 +0200

    Add a lerpnote function
    The goal of this is to clean up Generate() from a bunch of startFreq
    and endFreq variable declarations

--- internal/songgenerator/generate.go
+++ internal/songgenerator/generate.go
@@ -12,17 +12,24 @@ func GetSong(sampleRate float64) []int16 {
 	var voice1 []note
 	initialVolume := 0.5
 	voice1 = append(voice1, note{duration: 5, volume: initialVolume, name: "C4"})
-	startingFreq, _ := getFrequency("C4")
-	endingFreq, _ := getFrequency("Db4")
-	voice1 = append(voice1,
-		note{
-			duration:     3,
-			volume:       initialVolume,
-			frequency:    startingFreq,
-			frequencyEnd: &endingFreq,
-		},
-	)
+	voice1 = append(voice1, lerpNote(2, initialVolume, "C4", "Db4"))
 
 	samples := getSamples(voice1, float64(sampleRate))
 	return samples
+}
+
+func lerpNote(lerpDuration float64, volume float64, noteStart string, noteEnd string) note {
+	startingFreq, err := getFrequency(noteStart)
+	endFreq, err2 := getFrequency(noteEnd)
+	if err != nil || err2 != nil {
+		// Better to just panic and let the user know
+		panic("Error getting frequencies. Check note naming.")
+	}
+	return note{
+		duration:     lerpDuration,
+		volume:       volume,
+		frequency:    startingFreq,
+		frequencyEnd: &endFreq,
+	}
+
 }
